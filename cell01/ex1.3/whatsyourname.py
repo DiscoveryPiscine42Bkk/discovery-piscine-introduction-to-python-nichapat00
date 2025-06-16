af22f45451d3375494ebfbc8a27972dbb6825867
nichapat00 <anuchazarim2517@gmail.com>
2025-06-16 13:38:21 +0000

    ex1.3

---
+++ cell01/ex1.3/whatsyourname.py
@@ -0,0 +1,4 @@
+# whatyourname.py
+first_name = input("Hey, whats your first name? : ").strip()
+last_name = input("And your last name? : ").strip()
+print(f"well, pleased To meet you, {first_name} {last_name}.")

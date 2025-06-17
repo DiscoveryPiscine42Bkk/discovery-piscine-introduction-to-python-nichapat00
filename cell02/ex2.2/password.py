462102154e6d6c6ec35ae3b5416390f20e00c619
nichapat00 <anuchazarim2517@gmail.com>
2025-06-17 18:17:46 +0700

    Create password.py

---
+++ cell02/ex2.2/password.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Predefined password
+password = "Python is awesome"
+
+# Prompt the user to enter the password
+entered = input()
+
+# Check if the entered password matches
+if entered == password:
+    print("ACCESS GRANTED")
+else:
+    print("ACCESS DENIED")

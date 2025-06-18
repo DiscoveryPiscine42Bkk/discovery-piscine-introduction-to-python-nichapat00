f0f53020a38963749d93cbcbb6d2da22430f2044
nichapat00 <anuchazarim2517@gmail.com>
2025-06-18 05:45:53 +0000

    ff

---
+++ cell06/ex01/upcase_it.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+# Method definition for upcase_it
+def upcase_it(s):
+    return s.upper()  # ใช้ .upper() เพื่อแปลงข้อความเป็นตัวพิมพ์ใหญ่
+
+# Test the method by calling it
+print(upcase_it("hello"))

ac9dc0ee7f8e3f1ed67d31fadd2c6b95f175e113
nichapat00 <anuchazarim2517@gmail.com>
2025-06-18 04:44:15 +0000

    q

---
+++ cell05/ex04/parameters.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+import sys
+
+# Get the number of parameters passed (excluding the script name)
+num_parameters = len(sys.argv) - 1  # Subtract 1 to exclude the script name
+
+# Display the number of parameters
+print(f"Number of parameters: {num_parameters}.")

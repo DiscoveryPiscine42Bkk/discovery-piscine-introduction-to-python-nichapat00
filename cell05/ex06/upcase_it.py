e370550795b896b83d1643c73e63eebbf3a4cce9
nichapat00 <anuchazarim2517@gmail.com>
2025-06-18 04:49:33 +0000

    p

---
+++ cell05/ex06/upcase_it.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import sys
+
+# Check if there is exactly one parameter passed
+if len(sys.argv) == 2:
+    # Convert the parameter to uppercase and print it
+    print(sys.argv[1].upper())
+else:
+    # If not exactly one parameter, print "none"
+    print("none")
